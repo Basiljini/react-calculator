a1eae5c9f6b5055751999d9f57e4bff8166a95fe
Basiljini <basilj7thonnicka@gmail.com>
2023-10-31 18:08:45 +0530

    multiply

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Row, Col, Button, Form } from "react-bootstrap";
 const a = [
   {value:"AC", isOp:true},
- {value:"%", isOp:true},{value:"bk", isOp:true},{value:"/", isOp:true},{value:"7", isOp:false},{value:"8", isOp:false},{value:"9", isOp:false},{value:"x", isOp:true},{value:"4", isOp:false},{value:"5", isOp:false},{value:"6", isOp:false},{value:"-", isOp:true},{value:"1", isOp:false}, {value:"2", isOp:false}, {value:"3", isOp:false}, {value:"+", isOp:true}, {value:"0", isOp:false},, {value:"00", isOp:false},, {value:".", isOp:false},, {value:"=", isOp:true},
+ {value:"%", isOp:true},{value:"bk", isOp:true},{value:"/", isOp:true},{value:"7", isOp:false},{value:"8", isOp:false},{value:"9", isOp:false},{value:"*", isOp:true},{value:"4", isOp:false},{value:"5", isOp:false},{value:"6", isOp:false},{value:"-", isOp:true},{value:"1", isOp:false}, {value:"2", isOp:false}, {value:"3", isOp:false}, {value:"+", isOp:true}, {value:"0", isOp:false},, {value:"00", isOp:false},, {value:".", isOp:false},, {value:"=", isOp:true},
 ];
 
 function App() {
